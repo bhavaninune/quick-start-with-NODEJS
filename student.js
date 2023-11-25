ef62d5e38917daaf88469e6c8085bd3dca773e6f
bhavaninune <142501709+bhavaninune@users.noreply.github.com>
2023-11-25 06:08:47 +0530

    Add files via upload
    creating arrow function&student obj

---
+++ student.js
@@ -0,0 +1,11 @@
+let a=5;
+let b=6;
+const productOfTwoNumbers=()=>a*b;
+    console.log(productOfTwoNumbers());
+const student={
+    name:'bhanu',
+    age:24,
+    marks:50
+    //console.log(student);
+}
+console.log(student);
