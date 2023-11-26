05c854726bda608b84cd205b8e0a76e227b4071f
bhavaninune <142501709+bhavaninune@users.noreply.github.com>
2023-11-26 06:12:52 +0530

    Add files via upload
    server with using different if else statements

--- server.js
+++ server.js
@@ -1,11 +1,17 @@
 const http = require('http');
-const myName = 'bhanu';
 
 const server = http.createServer((req, res) => {
-    console.log(req);
-    res.end(myName); // Corrected statement and added quotes around 'myName'
+    if (req.url === '/home') {
+        res.end('Welcome home');
+    } else if (req.url === '/about') {
+        res.end('Welcome to About Us page');
+    } else if (req.url === '/node') {
+        res.end('Welcome to my Node Js project');
+    } else {
+        res.end('Hello! This is the default response.');
+    }
 });
 
-server.listen(4000, () => {
+server.listen(3000, () => {
     console.log('Server is running on http://localhost:3000');
 });
